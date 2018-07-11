295fad2c078e7f3f1c817649fefc3b60c2e09b6d
m.sawastian <m.sawastian@gmail.com>
2018-07-11 16:44:39 +0200

    POST path

--- server/server.js
+++ server/server.js
@@ -1,3 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (request, response) => {
+    const todo = new Todo({
+        text: request.body.text
+    });
+
+    todo.save().then(
+        document => response.send(document),
+        error => response.status(400).send(error)
+    )
+});
+
+app.listen(3000, () => {
+    console.log('Started on port 3000.')
+});
