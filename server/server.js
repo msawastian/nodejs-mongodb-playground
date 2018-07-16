012c33c2f8c951328441248054089873718bf79d
m.sawastian <m.sawastian@gmail.com>
2018-07-16 11:41:36 +0200

    GET w/ id path

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const {ObjectID} = require('mongodb');
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
@@ -25,6 +26,27 @@ app.get('/todos', (request, response) => {
         todos => response.send({todos}),
         error => response.status(400).send(error)
     )
+});
+
+app.get('/todos/:id', (request, response) => {
+    const id = request.params.id;
+
+    if (!ObjectID.isValid(id)) {
+         return response.status(404).send({
+            error: 'Not a valid ID'
+        });
+    }
+
+    Todo.findById(id)
+        .then(document => {
+            if (!document) {
+                return response.status(404).send();
+            }
+
+            response.send({document})
+        })
+        .catch(error => response.status(400).send())
+
 });
 
 app.listen(3000, () => {
