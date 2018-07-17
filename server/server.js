c0efa9227279b117fbbdce231e71195b1d5a3705
m.sawastian <m.sawastian@gmail.com>
2018-07-17 21:55:14 +0200

    User authentication

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const {ObjectID} = require('mongodb');
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
+const {authenticate} = require('./middleware/authenticate');
 
 const app = express();
 const port = process.env.PORT;
@@ -109,6 +110,10 @@ app.post('/users/', (request, response) => {
             response.header({'x-auth': token}).send(newUser)
         })
         .catch(error => response.send(error))
+});
+
+app.get('/users/me', authenticate, (request, response) => {
+    response.send(request.user)
 });
 
 
