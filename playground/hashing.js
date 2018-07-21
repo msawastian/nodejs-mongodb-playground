026551715ec6d8a806f9d25b23532cf6d683893b
Mateusz Sawastian <m.sawastian@gmail.com>
2018-07-21 18:53:25 +0200

    Passwords hashed and salted

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,16 +1,30 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
+let password = '123abc';
 
-const data = {
-    id: 4
-};
+bcrypt.genSalt(10, (error, salt) => {
+    bcrypt.hash(password, salt, (error, hash) => {
+        console.log(hash)
+    })
+});
 
-let token = jwt.sign(data, '123abc');
-console.log(token);
+const hashedPassword = '$2a$10$QFlM0aQrT/wbZJzYoc9vvOm57UP9JOmerl1LhgSruJupKR68mCj02';
 
-let decoded = jwt.verify(token, '123abc');
-console.log(decoded);
+bcrypt.compare(password, hashedPassword, (error, result) => {
+    console.log(result)
+});
+
+// const data = {
+//     id: 4
+// };
+//
+// let token = jwt.sign(data, '123abc');
+// console.log(token);
+//
+// let decoded = jwt.verify(token, '123abc');
+// console.log(decoded);
 
 // let message = 'Hello world!';
 // let hash = SHA256(message).toString();
