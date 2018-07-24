92d84c5baa6fdb9c263b1aa99f3d0f13063f45dc
m.sawastian <m.sawastian@gmail.com>
2018-07-24 21:13:09 +0200

    Config moved to json file

--- server/tests/seed/seed.js
+++ server/tests/seed/seed.js
@@ -14,7 +14,7 @@ const dummyUsers = [
         password: 'userOnePassword',
         tokens: [{
             access: 'auth',
-            token: jwt.sign({_id: userOneID, access: 'auth'}, 'abc123').toString()
+            token: jwt.sign({_id: userOneID, access: 'auth'}, process.env.JWT_SECRET).toString()
         }]
     },
     {
@@ -23,7 +23,7 @@ const dummyUsers = [
         password: 'userTwoPassword',
         tokens: [{
             access: 'auth',
-            token: jwt.sign({_id: userTwoID, access: 'auth'}, 'abc123').toString()
+            token: jwt.sign({_id: userTwoID, access: 'auth'}, process.env.JWT_SECRET).toString()
         }]
     }
 ];
